b6c2996be991e758043fc5e5ac2a9d6941478223
Tom Willmot <tom@humanmade.co.uk>
2012-01-20 19:20:09 +0000

    Bump

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WP Remote
 Description: Manage your WordPress site with <a href="https://wpremote.com/">WP Remote</a>. Deactivate to clear your API Key.
-Version: 2.0.5
+Version: 2.0.6
 Author: Human Made Limited
 Author URI: http://hmn.md/
 */
