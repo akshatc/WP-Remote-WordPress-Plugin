6f0c20aaa3c79e2248e7eadfcd35e1d9602b33f5
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-11-12 17:22:02 +0000

    space between ! and empty

--- wprp.compatability.php
+++ wprp.compatability.php
@@ -65,10 +65,10 @@ add_action( 'admin_notices', 'wprp_security_admin_notice' );
  */
 function wprp_dismissed_plugin_notice_check() {
 	
-	if ( !empty( $_GET['wpr_dismiss_plugin_warning'] ) ) {
 		wp_redirect( remove_query_arg( 'wpr_dismiss_plugin_warning' ) );
 
 		$dismissed = get_option( 'dismissed-plugins', array() );
+	if ( ! empty( $_GET['wpr_dismiss_plugin_warning'] ) ) {
 
 		$dismissed[] = $_GET['wpr_dismiss_plugin_warning'];
 
