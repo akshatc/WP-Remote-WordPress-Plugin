ef5a0ab899a5036f97daeb1cb934908d275fa685
Luke Stevenson <luke@lucanos.com>
2013-04-27 03:34:19 +1000

    Removed Domain-Check for Multiple Keys
    Now allows for an array of WP Remote API Keys. Will test each until a
    valid key is found.
    Added "Settings" link to Admin Notice and Plugin Listing to access
    Plugin Settings Page.
    Removed function which reset the (single) API Key on
    Activation/Deactivation of Plugin, as this can now be done at any time
    through the Settings Page.

--- wprp.admin.page.php
+++ wprp.admin.page.php
@@ -16,21 +16,16 @@ if( isset( $_POST['wprp_hidden'] ) && $_POST['wprp_hidden']=='Y' ){
   
   if( isset( $_POST['wprp_select'] ) )
     $wprp_select = $_POST['wprp_select'];
-  if( isset( $_POST['wprp_pattern'] ) )
-    $wprp_pattern = $_POST['wprp_pattern'];
   if( isset( $_POST['wprp_apikey'] ) )
     $wprp_apikey = $_POST['wprp_apikey'];
 
   $final = array();
   for( $i=0 , $c=max( count( $wprp_pattern ) , count( $wprp_apikey ) ) ; $i<$c ; $i++ ){
-    if( isset( $wprp_pattern[$i] ) && $wprp_pattern[$i] && isset( $wprp_apikey[$i] ) && $wprp_apikey[$i] && !isset( $wprp_select[$i] ) )
-      $final[$i] = array(
-        'pattern' => trim( $wprp_pattern[$i] ) ,
-        'apikey'  => preg_replace( '/[^0-9A-F]+/' , '' , $wprp_apikey[$i] )
-      );
+    if( isset( $wprp_apikey[$i] ) && $wprp_apikey[$i] && !isset( $wprp_select[$i] ) )
+      $final[$i] = preg_replace( '/[^0-9A-F]+/' , '' , $wprp_apikey[$i] );
   }
   
-  $updated = update_option( 'wpr_api_keychain' , $final );
+  $updated = update_option( 'wpr_api_keychain' , array_values( $final ) );
 
 }  
 
@@ -49,6 +44,8 @@ if( !is_null( $updated ) ){
 <?php
   }
 }
+
+$wprp = get_option( 'wpr_api_keychain' , array() );
 ?>
 			<form id="wprp_form" name="wprp_form" method="post">
         <input type="hidden" name="wprp_hidden" value="Y" />
@@ -56,35 +53,25 @@ if( !is_null( $updated ) ){
           <thead>
             <tr>
               <th scope="col" id="cb" class="manage-column column-cb check-column"></th>
-              <th scope="col" class="manage-column">Domain or RegExp Pattern</th>
               <th scope="col" class="manage-column">API Key</th>
             </tr>
           </thead>
           <tfoot>
             <tr>
               <th scope="col" class="manage-column column-cb check-column"></th>
-              <th scope="col" class="manage-column">Domain or RegExp Pattern</th>
               <th scope="col" class="manage-column">API Key</th>
             </tr>
           </tfoot>
           <tbody id="the-list">
 <?php
-
-$wprp = get_option( 'wpr_api_keychain' , array() );
-$domain = $_SERVER['HTTP_HOST'];
-
 foreach( $wprp as $k => $v ){
-  $matched = ( $v['pattern']==$domain || ( substr( $v['pattern'] , 0 , 1 )=='/' && @preg_match( $v['pattern'] , $domain ) ) );
 ?>
             <tr<?php echo ( $matched ? ' class="matched"' : '' ); ?>>
               <th scope="row" class="check-column">
                 <input type="checkbox" name="wprp_select[<?php echo $k; ?>]" id="wprp_<?php echo $k; ?>" value="<?php echo $k; ?>" />
               </th>
               <td>
-                <label for="wprp_pattern[<?php echo $k; ?>]"><input type="text" name="wprp_pattern[<?php echo $k; ?>]" id="wprp_pattern_<?php echo $k; ?>" value="<?php echo ( isset( $v['pattern'] ) ? $v['pattern'] : '' ); ?>" /></label>
-              </td>
-              <td>
-                <label for="wprp_apikey[<?php echo $k; ?>]"><input type="text" name="wprp_apikey[<?php echo $k; ?>]" id="wprp_apikey_<?php echo $k; ?>" value="<?php echo ( isset( $v['apikey'] ) ? $v['apikey'] : '' ); ?>" /></label>
+                <label for="wprp_apikey[<?php echo $k; ?>]"><input type="text" name="wprp_apikey[<?php echo $k; ?>]" id="wprp_apikey_<?php echo $k; ?>" value="<?php echo ( isset( $v ) && is_string( $v ) ? $v : '' ); ?>" /></label>
               </td>
             </tr>
 <?php
@@ -93,9 +80,6 @@ $k = count( $wprp );
 ?>
             <tr>
               <th scope="row">&nbsp;</th>
-              <td>
-                <label for="wprp_pattern[<?php echo $k; ?>]"><input type="text" name="wprp_pattern[<?php echo $k; ?>]" id="wprp_pattern_<?php echo $k; ?>" value="" /></label>
-              </td>
               <td>
                 <label for="wprp_apikey[<?php echo $k; ?>]"><input type="text" name="wprp_apikey[<?php echo $k; ?>]" id="wprp_apikey_<?php echo $k; ?>" value="" /></label>
               </td>
