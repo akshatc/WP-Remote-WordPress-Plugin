2c469a8b3eb6c4898352af4a3e237d1eec140364
Daniel Bachhuber <d@danielbachhuber.com>
2013-12-21 08:27:13 -0800

    Because `getmypid()` can return an empty string and there can still be a valid backup process, make this a strict comparison

--- wprp.backups.php
+++ wprp.backups.php
@@ -527,7 +527,7 @@ class WPRP_Backups extends WPRP_HM_Backup {
 	private function is_backup_still_running() {
 
 		// Check whether there's supposed to be a backup in progress
-		if ( false == ( $process_id = $this->get_backup_process_id() ) )
+		if ( false === ( $process_id = $this->get_backup_process_id() ) )
 			return false;
 
 		$time_to_wait = 120;
