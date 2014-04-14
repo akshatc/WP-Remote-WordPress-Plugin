2a99e944b440d5a2a465674cec393deccfa87fe2
Owain Cuvelier <owain@hmn.md>
2014-04-14 15:46:28 +1000

    updated WP Better Security name to iThemes, updated troubleshoot link

--- wprp.compatability.php
+++ wprp.compatability.php
@@ -8,7 +8,7 @@ function wprp_get_incompatible_plugins() {
 	// Plugins to check for.
 	$security_plugins = array(
 		__( 'Wordfence Security', 'wpremote' ),
-		__( 'Better WP Security', 'wpremote' ),
+		__( 'iThemes Security', 'wpremote' ),
 		__( 'Wordpress Firewall 2', 'wpremote' )
 	);
 
@@ -50,7 +50,7 @@ function wprp_security_admin_notice() {
 
 				<?php _e( 'The plugin', 'wpremote' );?> <strong><?php echo esc_attr( $plugin_name ); ?></strong> <?php _e( 'may cause issues with WP Remote.', 'wpremote' ); ?>
 
-				<a href="https://wpremote.com/support-center/"> <?php _e( 'Click here for instructions on how to resolve this issue', 'wpremote' ); ?> </a>
+				<a href="https://wpremote.com/support-center/troubleshooting/incorrect-version-numbers-false-positives/ "> <?php _e( 'Click here for instructions on how to resolve this issue', 'wpremote' ); ?> </a>
 
 			</p>
 
